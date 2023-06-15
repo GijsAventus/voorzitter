41a69c36a23283821d5d63d8d8d645b8d88c94fb
GijsAventus <gijs.bal@student.aventus.nl>
2023-06-15 08:41:12 +0200

    poll

--- commands/createpoll.js
+++ commands/createpoll.js
@@ -1,20 +1,51 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('@discordjs/builders');
+const { 
+    PermissionFlagsBits, 
+    ChatInputCommandInteraction, 
+    SlashCommandBuilder, 
+    EmbedBuilder, 
+    ActionRowBuilder, 
+    ButtonBuilder, 
+    ButtonStyle } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('createpoll')
     .setDescription('Makes a new poll')
+    .setDMPermission(false)
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
     .addStringOption(option => 
         option.setName('question')
         .setDescription('question for the poll')
         .setRequired(true)
         )
     ,
+    
+    /**
+     * @param {ChatInputCommandInteraction} interaction
+     */
+    
     async execute(interaction)
     {
-        await interaction.reply({
-            content: 'Poll started',
-            ephemeral: true
-        });
+        const pollQuestion = interaction.options.getString('question');
+        
+        const pollEmbed = new EmbedBuilder()
+        .setTitle('titel')
+        .setDescription(pollQuestion)
+        .addFields(
+            {name:'Yes', value:'0', inline:true},
+            {name:'No', value:'0', inline:true}
+        )
+        .setColor(0xAA4444);
+
+        const replyObject = await interaction.reply({embeds:[pollEmbed], fetchReply:true})
+
+        const pollButtons = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder().setLabel('Yes').setCustomId(`Poll-Yes-${replyObject.id}`).setStyle(ButtonStyle.Success),
+            new ButtonBuilder().setLabel('No').setCustomId(`Poll-No-${replyObject.id}`).setStyle(ButtonStyle.Danger)
+        )
+        
+        interaction.editReply({components:[pollButtons]})
+
     }
 };
---
+++ commands/embedtest.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('embedtest')
+    .setDescription('Testing for embeds')
+    ,
+    async execute(interaction)
+    {
+        const embedTest = new EmbedBuilder()
+        .setTitle('Test Embed')
+        .setDescription('Voor embed testing zooi')
+        .addFields(
+            {name:'Test field 1', value:'description 1', inline: true},
+            {name:'Test field 2', value:'description 2', inline: true},
+        )
+        .setColor(0x89B0AE);
+        interaction.reply({embeds: [embedTest]});
+    }
+};
